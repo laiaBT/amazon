52e8e05cdf2df77c3fb7b7cf87f7ef7452945c86
laiaBT <84039069+laiaBT@users.noreply.github.com>
2021-05-27 18:00:19 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+game.splash("A l ataque")
+effects.smiles.startScreenEffect()
+let Amazon = sprites.create(img`
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 7 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . 8 7 . . . . . . . 
+    . . . . . . 8 8 5 6 . . . . . . 
+    . . . . . . 8 7 5 6 . . . . . . 
+    . . . . . c c c 6 6 6 . . . . . 
+    . . . . 8 8 7 7 7 5 6 6 . . . . 
+    . . 8 f f f c c 6 6 f f 6 6 . . 
+    . 8 8 8 8 6 6 7 7 7 7 5 7 6 6 . 
+    8 8 8 8 8 8 6 6 7 7 7 5 7 7 6 6 
+    8 8 8 8 8 8 6 6 7 7 7 7 5 7 6 6 
+    `, SpriteKind.Player)
+Amazon.setPosition(82, 59)
+controller.moveSprite(Amazon, 100, 100)
+Amazon.setStayInScreen(true)
+game.onUpdateInterval(500, function () {
+	
+})
